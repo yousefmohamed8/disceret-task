c7d4068f58550ce7ad7b69055118c036bf9626ba
yousefmohamed8 <121302294+yousefmohamed8@users.noreply.github.com>
2022-12-24 00:34:59 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
-﻿int n1, n2, flag = 0;
+﻿int n1, n2, sum = 0;
 Console.Write("Enter Frist number: ");
 n1 = int.Parse(Console.ReadLine());
 Console.Write("Enter second number: ");
 n2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Prime numbers  are: ");
+Console.WriteLine("Perfect numbers  are: ");
 Console.WriteLine("-----------------------------------------------------");
-for (int i = n1; i < n2; i++)
+for (int i = n1; i <= n2; i++)
 {
-    flag = 0;
-    if (i > 1)
-    {
-        for (int j = 2; j < i; j++)
+    sum = 0;
+  
+    
+        for (int j = 1; j < i; j++)
         {
-            if (i % j == 0)
+            if (i % j == 0)sum=sum + j;
             {
-                flag = 1;
-                continue;
+               
+             
             }
         }
-        if (flag == 0)
+        if (sum == i)
         {
-            Console.WriteLine(i);
-        }
+        Console.WriteLine(i);
     }
+       
+    
 }
 
